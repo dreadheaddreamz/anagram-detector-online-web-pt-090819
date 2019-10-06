f336a59b7fef62e3ad2e917bdb9c384486451978
James Batsell <jamesbatsell05@gmail.com>
2019-10-06 02:27:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,9 @@ class Anagram
   
   def match(other_word)
     other_word.select do |w|
-    other_word.split("").sort == w.split.
+    other_word.split("").sort == w.split("").sort
   end
+end
   
   
 end
